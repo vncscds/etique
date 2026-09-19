22b06368d47fc5eaa67f504de1c8b39870afbb35
Vinicius <oldz.registros@gmail.com>
2026-09-19 14:31:07 -0300

    ref: enhance bottom sheet custom hook

--- src/shared/components/hooks/useBottomSheet.ts
+++ src/shared/components/hooks/useBottomSheet.ts
@@ -1,10 +1,24 @@
-import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { useRef } from "react";
+import { type BottomSheetModal, useBottomSheetModal } from '@gorhom/bottom-sheet';
+import React, { useRef } from 'react';
+import { Keyboard } from 'react-native';
 
-export default function useBottomSheet() {
-  const bottomSheetRef = useRef<BottomSheetModal>(null);
+export default function useBottomSheet<T>() {
+	const bottomSheetModalRef = useRef<BottomSheetModal<T>>(null);
+	const bottomSheetModal = useBottomSheetModal();
 
-  return {
-    bottomSheetRef
-  };
+	const open = React.useCallback((data?: T) => {
+		bottomSheetModalRef.current?.present(data);
+	}, []);
+
+	const close = React.useCallback(() => {
+		Keyboard.dismiss();
+		bottomSheetModalRef.current?.close();
+	}, []);
+
+	return {
+		ref: bottomSheetModalRef,
+
+		open,
+		close,
+	};
 }
