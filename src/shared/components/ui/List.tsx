a9bf1295c753e9f0d7c29fea5c37ae6717466edc
Vinicius <oldz.registros@gmail.com>
2026-09-19 14:30:54 -0300

    feat: add search bar in List

--- src/shared/components/ui/List.tsx
+++ src/shared/components/ui/List.tsx
@@ -1,22 +1,42 @@
-import { FlashList, FlashListProps } from "@shopify/flash-list";
-import { StyleSheet } from "react-native";
+import useKeyboardHeight from '@/shared/hooks/useKeyboard';
+import useSearch from '@/shared/hooks/useSearch';
+import { FlashList, type FlashListProps } from '@shopify/flash-list';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import SearchInput from './SearchInput';
 
-export default function List<T>({ contentContainerStyle, ...props }: FlashListProps<T>) {
-  return (
-    <FlashList
-      contentContainerStyle={[{ flex: 1 }, contentContainerStyle]}
-      ListEmptyComponentStyle={styles.listEmptyComponentStyle}
-      keyboardShouldPersistTaps="always"
-      showsVerticalScrollIndicator={false}
-      {...props}
-    />
-  )
+type ListProps<T> = {
+	data: Array<T>;
+	searchSettings?: {
+		searchKeys: ReadonlyArray<(keyof T & string) | string> | string[];
+	};
+} & FlashListProps<T>;
+
+export default function List<T>({ data, searchSettings, contentContainerStyle, ...props }: ListProps<T>) {
+	const { result, state } = useSearch<T>({ data, keys: searchSettings?.searchKeys });
+
+	const keyboardHeight = useKeyboardHeight();
+	const bottomPadding = Math.max(keyboardHeight * 0.8, 16);
+
+	return (
+		<React.Fragment>
+			<SearchInput placeholder="Pesquisar..." value={state.query} onChangeText={state.setQuery} />
+			<FlashList
+				data={result}
+				contentContainerStyle={{ ...StyleSheet.flatten(contentContainerStyle), paddingBottom: bottomPadding }}
+				ListEmptyComponentStyle={styles.listEmptyComponentStyle}
+				keyboardShouldPersistTaps="always"
+				showsVerticalScrollIndicator={false}
+				{...props}
+			/>
+		</React.Fragment>
+	);
 }
 
 const styles = StyleSheet.create({
-  listEmptyComponentStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+	listEmptyComponentStyle: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
